85d00485cd27f543d80d199193cb0f7505b8302a
Joseph Illana <joseph.illana.j@gmail.com>
2026-05-12 16:26:32 -0600

    Se agrega timeout al transporter de nodemailer

--- src/services/email.service.ts
+++ src/services/email.service.ts
@@ -7,6 +7,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 3000,
+  socketTimeout: 3000,
 });
 
 export async function sendLowStockAlert(product: Product, recipientEmails: string[]): Promise<void> {
